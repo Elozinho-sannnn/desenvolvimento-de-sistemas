6d028b0605b6a9a74ba1b93b2ab1deddc19fd3ba
Elozinho-sannnn <githubchannelzinho@gmail.com>
2025-03-14 11:51:33 -0300

    Add files via upload

---
+++ Divida/eelo.py
@@ -0,0 +1,18 @@
+custos = [1000, 500 ,700 , 300]
+ganho = [4500, 950]
+
+def somatorio(lista):
+    contador= 0
+    soma= 0
+    while (contador < len(lista)):
+        soma= soma+ lista[contador]
+        contador= contador+1 
+    return soma 
+
+somacusto = somatorio(custos)
+somaganho = somatorio(ganho)
+
+print ("O custos foi de:", somacusto)
+print ("O ganho foi de: ", somaganho)
+print ("O Saldo foi de: ", somaganho - somacusto)
+
